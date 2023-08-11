845bbd646519fb62ca9f5196e343bacf9314dfc5
ALEKSEY <88752711+DrFess@users.noreply.github.com>
2023-08-11 20:11:50 +0700

    for deploy

--- db/settings.py
+++ db/settings.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql+psycopg://postgres:postgres@193.124.125.45:5432/burns")
+engine = create_engine("postgresql+psycopg://postgres:postgres@localhost:5432/burns")
 
